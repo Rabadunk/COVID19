12890140bf4106012c3f55ae040efde18c031ff9
Jonathan Alphono <jono@Jonathans-MacBook-Pro.local>
2020-03-24 21:58:12 +1300

    Updates

---
+++ precache-manifest.b03540c8bcc7094cea559eb6cb82b9ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed9afdfe15fe730e5b3afa54e20bcff9",
+    "url": "/COVID19/index.html"
+  },
+  {
+    "revision": "ac1b00ba963570b2f316",
+    "url": "/COVID19/static/css/main.ee5543b9.chunk.css"
+  },
+  {
+    "revision": "4ab93e0293955fde2764",
+    "url": "/COVID19/static/js/2.9e3be2f6.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/COVID19/static/js/2.9e3be2f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac1b00ba963570b2f316",
+    "url": "/COVID19/static/js/main.112b4624.chunk.js"
+  },
+  {
+    "revision": "ebbc3c029553521e1fdd",
+    "url": "/COVID19/static/js/runtime-main.b6cf2ffe.js"
+  }
+]);
