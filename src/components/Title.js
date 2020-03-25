614118e01c0c9c0197edcb4532349e817bda6819
Jonathan <rabadunk@gmail.com>
2020-03-26 12:41:13 +1300

    changed title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { FiAlertTriangle } from "react-icons/fi";
+import React from 'react'
+import { FiAlertTriangle } from 'react-icons/fi'
 
-function Title() {
-    return (
-      <div className="Title">
-        <h1> <FiAlertTriangle /> COVID-19 <FiAlertTriangle/> </h1>
-      </div>
-    );
-  }
-  
-  export default Title;
+function Title () {
+  return (
+    <div className='Title'>
+      <h1>
+        COVID-19 <FiAlertTriangle />
+      </h1>
+    </div>
+  )
+}
+
+export default Title
