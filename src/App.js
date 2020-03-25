8a1221241316d9632ef156d09e3fe18d10940090
Jonathan Alphono <jono@Jonathans-MacBook-Pro.local>
2020-03-26 02:50:52 +1300

    removed redundant imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Title from './components/Title'
-import AgeChart from './components/AgeChart';
-import GenderChart from './components/GenderChart';
 import Map from './components/Map'
 import './App.css';
 
