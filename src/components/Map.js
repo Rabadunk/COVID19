32d6ecbc2ef4c17a2f250e553f7690cd7f4f9394
Jonathan Alphono <jono@Jonathans-MacBook-Pro.local>
2020-03-26 19:12:48 +1300

    added zooming to map as feature

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,14 @@
-import React, {useState} from "react";
-import ReactMapGL, { Marker } from "react-map-gl"
+import React, {useState, useRef} from "react";
+import ReactMapGL, { Marker, FlyToInterpolator } from "react-map-gl";
+import userSupercluster from "use-supercluster";
+
+
 import data from '../data/data.json'
 import locations from '../data/locations.json'
+
+import { MdZoomOutMap } from "react-icons/md";
 import { Button } from "react-bootstrap";
+import useSupercluster from "use-supercluster";
 
 
 export default function Map() {
@@ -14,10 +20,11 @@ export default function Map() {
         zoom: 4.418
     });
 
+    const mapRef = useRef()
+
+    // Extracting local data for map
     let mapJuice = {
     }
-
-
 
     data.forEach(async (element) => {
     
@@ -37,18 +44,106 @@ export default function Map() {
         })
     }
 
+    // Clustering
+    // Format for clusters
+    const points = mapData.map( place => ({
+        type: "Feater",
+        properties: {
+            cluser: false,
+            placeId: place.data[0],
+            count: place.data[1].count
+        },
+        geometry: {
+            type: "Point",
+            coordinates: [place.data[1].location.lng, place.data[1].location.lat]
+        }
+    }));
+
+    // Get map bounds
+    const bounds= mapRef.current ? mapRef.current.getMap().getBounds().toArray().flat() : null;
+
+    // Get clusters
+    const {clusters, supercluster} = useSupercluster({
+        points,
+        zoom: viewport.zoom,
+        bounds,
+        options: { radius: 40 , maxZoom: 12}
+    })
+
+    console.log(clusters);
+
     return (
     // Set a height on the map so it will display
         <ReactMapGL {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAP_KEY}
-        onViewportChange={ viewport => { setViewport(viewport)}}>
+        onViewportChange={ viewport => { setViewport(viewport)}}
+        maxZoom={10}
+        ref={mapRef}>
+
+            <Button className="Zoom-Out" onClick={ () => {
+                setViewport({
+                    latitude: -41.51128245580759,
+                    longitude: 172.72407079826075,
+                    width: '100%',
+                    height: '100vh',
+                    zoom: 4.418,
+                    transitionInterpolator: new FlyToInterpolator({
+                        speed: 1
+                    }),
+                    transitionDuration: "auto"     
+                })}
+            }>
+                <MdZoomOutMap/>
+            
+            </Button>
 
             {
-                mapData.map(place => (
-                    <Marker key={place.data[0]} latitude={place.data[1].location.lat} longitude={place.data[1].location.lng}>
+                clusters.map(cluster => {
 
-                        <Button variant="warning" className="Case-Marker">{place.data[1].count}</Button>
-                    </Marker>
-                ))
+                    const [longitude, latitude] = cluster.geometry.coordinates;
+                    const {cluster: isCluster, point_count: pointCount} = cluster.properties;
+
+                    if(isCluster) {
+
+                        return(
+                            <Marker key={cluster.id} latitude={latitude} longitude={longitude}>
+                                <Button variant="warning" className="Case-Marker" 
+                                style={{width: `${50 + 20*(pointCount / points.length)}px`, height: `${50 + 20*(pointCount / points.length)}px`}}
+                                onClick={ () => {
+                                    const expansionZoom = Math.min(supercluster.getClusterExpansionZoom(cluster.id), 12);
+                                    setViewport({
+                                        ...viewport,
+                                        latitude,
+                                        longitude,
+                                        zoom: expansionZoom,
+                                        transitionInterpolator: new FlyToInterpolator({
+                                            speed: 1
+                                        }),
+                                        transitionDuration: "auto"
+                                    })
+                                }}>{pointCount}</Button>
+                            </Marker>
+                        );
+                    }
+
+                    return(
+                        <Marker key={cluster.properties.placeId} latitude={latitude} longitude={longitude}>
+                            <Button variant="warning" className="Case-Marker"
+                                    style={{width: `${50 + 20  * (cluster.properties.count / points.length)}px`, height: `${50 + 20* (cluster.properties.count / points.length)}px`}}
+                                    onClick={ () => {
+                                        setViewport({
+                                            ...viewport,
+                                            latitude,
+                                            longitude,
+                                            zoom: 12,
+                                            transitionInterpolator: new FlyToInterpolator({
+                                                speed: 1
+                                            }),
+                                            transitionDuration: "auto"
+                                        })
+                                    }}>{cluster.properties.count}</Button>
+                        </Marker>
+                    )
+                })
             }
 
             {
