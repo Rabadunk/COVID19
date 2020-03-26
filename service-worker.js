31c7871b19526ab6e15489e57e3f3a5af59cfd24
Rabadunk <31195359+Rabadunk@users.noreply.github.com>
2020-03-26 06:15:17 +0000

    Deploying to gh-pages from  - 32d6ecbc2ef4c17a2f250e553f7690cd7f4f9394 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/COVID19/precache-manifest.6e1d5faccddda11c3f63f6f2b631181a.js"
+  "/COVID19/precache-manifest.274e35007d6f81149805cfffd721f59e.js"
 );
 
 self.addEventListener('message', (event) => {
